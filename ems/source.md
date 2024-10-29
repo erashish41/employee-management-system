a9b704403c041007e2440ce234046c9d0c2492e2
Ashish Bhardwaj <erashish41@gmail.com>
2024-10-30 00:00:26 +0530

    updated

--- ems/source.md
+++ ems/source.md
@@ -1,18 +1,26 @@
 **website page flow**
 - src
-    -component
-        -Auth
-            -Login.jsx
-        -Dashboard
-            -AdminDashboard.jsx
-            -EmloyeeDashboard.jsx
-        -TaskList
-            -AcceptTask.jsx
-            -CompleteTask.jsx
-            -FailedTask.jsx
-            -NewTask.jsx
+    - component
+        - Auth
+            - Login.jsx
+        - Dashboard
+            - AdminDashboard.jsx
+            - EmloyeeDashboard.jsx
+        - TaskList
+            - AcceptTask.jsx
+            - CompleteTask.jsx
+            - FailedTask.jsx
+            - NewTask.jsx
     - context
-        -AuthContext.jsx
-        -TaskContext.jsx
+        - AuthContext.jsx
+        - TaskContext.jsx
     - utils
-        -LocalStorage.jsx
+        - LocalStorage.jsx
+
+
+- Login.jsx- Tailwind Css added in it
+-h-screen and w-screen used to set the height and weight of element to full height and weight 
+    of viewPoint
+-h-screen is same as height: 100vh and weight: 100vw
+
+-rounded-xl
--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 border-red-600'>
+        <div className='border-2 border-emerald-600 p-20 rounded-xl'>
             <form className='flex flex-col items-center justify-center'>
                 <input 
-                className='text-black outline-none border-2 border-emerald-600 px-3 py-3 mx-2 my-2 text-xl rounded-full' 
+                className=' outline-none bg-transparent placeholder:text-gray-300 border-2 border-emerald-600 px-3 py-3 mx-2 my-2 text-xl rounded-full' 
                 type='email' 
                 placeholder='email'/>
-                <input type='password' placeholder='password'/>
-                <button type='submit'>Log in the page</button>
+                <input
+                className=' outline-none bg-transparent placeholder:text-gray-300 border-2 border-emerald-600 px-3 py-3 mx-2 my-2 text-xl rounded-full'
+                type='password' placeholder='password'/>
+                <button 
+                className='text-white outline-none border-none bg-emerald-600 px-3 py-3 mx-2 my-2 text-xl rounded-full'
+                type='submit'>Log in</button>
             </form>
         </div>
     </div>
