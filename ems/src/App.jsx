f368b30663fcf76ff4250aa7313f51aabbfcfb06
Ashish Bhardwaj <erashish41@gmail.com>
2024-10-29 23:35:20 +0530

    form included

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Login from './components/Auth/Login'
+import "./index.css";
 
 const App = () => {
   return (
--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex h-screen w-screen items-center justify-center'>
         <div className='border-2 border-red-600'>
-            <form>
-                <input type='email' placeholder='email'/>
+            <form className='flex flex-col items-center justify-center'>
+                <input 
+                className='text-black border-2 border-emerald-600 px-3 py-3 mx-2 my-2 text-xl rounded-full' 
+                type='email' 
+                placeholder='email'/>
                 <input type='password' placeholder='password'/>
-                <button type='submit'>submit</button>
+                <button type='submit'>Log in the page</button>
             </form>
         </div>
     </div>
