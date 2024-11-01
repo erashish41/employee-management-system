a44fdf0ab48d68ff358f44414c22bba34b220b92
Ashish Bhardwaj <erashish41@gmail.com>
2024-11-01 23:02:14 +0530

    TaskList.jsx added

--- ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
 import TaskItemList from '../others/TaskItemList'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1C1C1C]'>
         <Header />
         <TaskItemList />
+        <TaskList />
     </div>
   )
 }
---
+++ ems/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='mt-10 py-50 overflow-x-auto bg-red-300 flex items-center flex-nowrap justify-start gap-5 h-[55%] w-full'>
+        <div className='flex-shrink-0 flex justify-between mt-10 p-3'>
+            <h2 className=' bg-red-600 rounded font-semibold text-l'>High</h2>
+            <h3 className='font-medium text-l'>20 June 2024</h3>
+        </div>
+        <div>
+            <h1 className='text-3xl'>Make your Employement Database</h1>
+            <p className='text-xl'>An employee management system or EMS is a tool that helps improve employee satisfaction and productivity to help a company achieve their overall goals. These tools help monitor, assess and control employees' working hours and efficiently utilise human resources.</p>
+        </div>
+        <div className='flex-shrink-0 flex justify-between mt-10 p-3'>
+            <h2 className=' bg-red-600 rounded font-semibold text-l'>High</h2>
+            <h3 className='font-medium text-l'>20 June 2024</h3>
+        </div>
+        <div>
+            <h1 className='text-3xl'>Make your Employement Database</h1>
+            <p className='text-xl'>An employee management system or EMS is a tool that helps improve employee satisfaction and productivity to help a company achieve their overall goals. These tools help monitor, assess and control employees' working hours and efficiently utilise human resources.</p>
+        </div>
+        <div className='flex-shrink-0 flex justify-between mt-10 p-3'>
+            <h2 className=' bg-red-600 rounded font-semibold text-l'>High</h2>
+            <h3 className='font-medium text-l'>20 June 2024</h3>
+        </div>
+        <div>
+            <h1 className='text-3xl'>Make your Employement Database</h1>
+            <p className='text-xl'>An employee management system or EMS is a tool that helps improve employee satisfaction and productivity to help a company achieve their overall goals. These tools help monitor, assess and control employees' working hours and efficiently utilise human resources.</p>
+        </div>
+        <div className='flex-shrink-0 flex justify-between mt-10 p-3'>
+            <h2 className=' bg-red-600 rounded font-semibold text-l'>High</h2>
+            <h3 className='font-medium text-l'>20 June 2024</h3>
+        </div>
+        <div>
+            <h1 className='text-3xl'>Make your Employement Database</h1>
+            <p className='text-xl'>An employee management system or EMS is a tool that helps improve employee satisfaction and productivity to help a company achieve their overall goals. These tools help monitor, assess and control employees' working hours and efficiently utilise human resources.</p>
+        </div>
+    </div>
+  )
+}
+
+export default TaskList
