52aefa53ae9a51a92a35685c651703eb2c52ab12
Ashish Bhardwaj <erashish41@gmail.com>
2024-11-02 13:10:14 +0530

    CreateTask.jsx updated

--- ems/source.md
+++ ems/source.md
@@ -23,7 +23,8 @@
         - LocalStorage.jsx
 
 
-1. Login.jsx: Tailwind Css added in it
+1. Login.jsx is connected to App.jsx
+Login.jsx: Tailwind Css added in it
 - flex-col: flex-col class in Tailwind CSS applies flex-direction: column;, which makes flex 
     items stack vertically instead of horizontally
 - h-screen and w-screen used to set the height and weight of element to full height and weight 
@@ -36,7 +37,7 @@
 - 2 way binding: Two-way data binding is like a bridge that connects your form components and
      the application's state.
 
-2. EmloyeeDashboard.jsx: 
+2. EmloyeeDashboard.jsx is connected to App.jsx
 - Header.jsx is added and used to make header of website.
 - TaskItemList.jsx is added and used to make content of Employee with TaskItemList.jsx.
 - TaskList.jsx is added and used to make tasks.
@@ -48,6 +49,9 @@
 -mb-2: margin bottom
 -leading-5: This class set the line-height at 1.25rem. It depends on the height of content
 
-3. AdminDashboard.jsx: Header.jsx is added 
+3. AdminDashboard.jsx is connected to App.jsx 
+AdminDashboard.jsx: Header.jsx is added 
+
 - CreateTask.jsx is added
 -mb-0.5	margin-bottom: 0.125rem; /* 2px */
+-w-full: This class means the width is set to full.
--- ems/src/components/others/CreateTask.jsx
+++ ems/src/components/others/CreateTask.jsx
@@ -3,30 +3,30 @@ import React from 'react'
 const CreateTask = () => {
   return (
         <div className='p-5 mt-7 bg-[#1c1c1c] rounded'>
-          <form className='flex items-start justify-between w-full bg-red-200'>
+          <form className='flex items-start justify-between w-full flex-wrap'>
             <div className='w-1/2'>
                 <div>
                     <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
-                    <input type='text' placeholder='Make a UI design'/>
+                    <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='text' placeholder='Make a UI design'/>
                 </div>
                 <div>
-                    <h3>Date</h3>
-                    <input type='date' />
+                    <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
+                    <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='date' />
                 </div>
                 <div>
-                    <h3>Assign to</h3>
-                    <input type='text' placeholder='employee name'/>
+                    <h3 className='text-sm text-gray-300 mb-0.5'>Assign to</h3>
+                    <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='text' placeholder='employee name'/>
                 </div>
                 <div>
-                    <h3>Category</h3>
-                    <input type='text' placeholder='design, dev, etc..'/>
+                    <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
+                    <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='text' placeholder='design, dev, etc..'/>
                 </div>
             </div>
 
-            <div>
-                <h3>Description</h3>
-                <textarea name='' id='' cols='30' rows='10'></textarea>
-                <button>Create task</button>
+            <div className='flex items-start flex-col'>
+                <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
+                <textarea className='w-full h-44 text-sm py-1 px-2 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' name='' id='' cols='30' rows='10'></textarea>
+                <button className='bg-emerald-500 py-3 px-2 w-full hover:bg-emerald-700 rounded text-sm mt-4 mb-2'>Create task</button>
             </div>
           </form>
         </div>
